524e1c8d4484cd4a9ef17c83de90ebdaa8cf8ae5
ZorEl212 <yabsirad212@gmail.com>
2023-10-31 14:02:52 -0400

    ginkgo: Setup Retrofit Partitions [2/2]
    Co-authored-by: Arian <arian.kulmer@web.de>
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- lineage_ginkgo.mk
+++ lineage_ginkgo.mk
@@ -14,6 +14,11 @@ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 # Inherit from ginkgo device
 $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 
+# Retrofit
+#PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+PRODUCT_RETROFIT_DYNAMIC_PARTITIONS := true
+
 PRODUCT_NAME := lineage_ginkgo
 PRODUCT_DEVICE := ginkgo
 PRODUCT_MANUFACTURER := Xiaomi
