5a7d0839f766d0b3ad2e62123b8aa89eceacb268
Flopster101 <nahuelgomez329@gmail.com>
2025-03-10 12:31:48 +0100

    ginkgo: Initialize EvolutionX
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- device.mk
+++ device.mk
@@ -71,7 +71,8 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(DEVICE_PATH)/overlay \
-    $(DEVICE_PATH)/overlay-lineage
+    $(DEVICE_PATH)/overlay-lineage \
+    $(DEVICE_PATH)/overlay-evolution
 
 PRODUCT_PACKAGES += \
     ApertureGinkgo \
