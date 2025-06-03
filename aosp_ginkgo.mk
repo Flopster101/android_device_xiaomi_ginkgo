ac141a7632d626de3982790e47aada494e1bfd6c
Flopster101 <nahuelgomez329@gmail.com>
2025-06-02 21:26:10 -0300

    ginkgo: bringup PixelOS fifteen
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- aosp_ginkgo.mk
+++ aosp_ginkgo.mk
@@ -8,18 +8,27 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from ginkgo device
 $(call inherit-product, device/xiaomi/ginkgo/device.mk)
 
-PRODUCT_NAME := lineage_ginkgo
+# PixelOS flags
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := aosp_ginkgo
 PRODUCT_DEVICE := ginkgo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 8
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+# Recovery
+TARGET_USES_AOSP_RECOVERY := true
 
 BUILD_FINGERPRINT := xiaomi/ginkgo_eea/ginkgo:11/RKQ1.201004.002/V12.5.12.0.RCOEUXM:user/release-keys
