pa-gr/device_xiaomi_ginkgo

ginkgo: parts: Add support for LCD HBM and CABC
 * added preferences and qs tiles
 * enabled cabc by default
 * addressed sepolicy denials
 * kanged hbm icon from realme parts
 * cabc icon by wstxda

Signed-off-by: Adithya R <gh0strider.2k18.reborn@gmail.com>

device.mk
@@ -134,6 +134,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiParts
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/parts/init.xiaomiparts.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/init.xiaomiparts.rc
+
 # Perf
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/perf/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf \

Flopster101/android_device_xiaomi_ginkgo

Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

device.mk
@@ -73,6 +73,9 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_PACKAGES += \
     LineageSDKGinkgo
 
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/parts/init.xiaomiparts.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/init.xiaomiparts.rc
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 28
 