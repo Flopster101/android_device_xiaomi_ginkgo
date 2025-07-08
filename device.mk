c8c32e4a617e3da9c036524cae65b3ea75dead29
Koushik Dey <kousickdey3@gmail.com>
2025-07-08 23:13:51 +0530

    ginkgo: Disable Matlog
    * Disable Matlog if the build variant is 'user' to prevent log access in production builds
    
    Signed-off-by: Koushik Dey <kousickdey3@gmail.com>
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>
    Change-Id: I1d4a0ecdba1f029340e3ca2486e7a663a58c753b

--- device.mk
+++ device.mk
@@ -51,6 +51,11 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl \
     $(DEVICE_PATH)/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-goodix.kl
 
+# Matlog
+ifeq ($(TARGET_BUILD_VARIANT),user)
+TARGET_DISABLE_MATLOG := true
+endif
+
 # NFC
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \
