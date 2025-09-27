592c577f1b915404f260c3c0ba15acbd04f276a6
Flopster101 <nahuelgomez329@gmail.com>
2025-09-27 18:03:18 -0300

    ginkgo: Add Settings maintainer patch
    Modified version of:
    * https://github.com/mytja/treble_pixelos_patches/blob/sixteen/personal/platform_packages_apps_Settings/0001-Settings-Add-Maintainer-string-into-device-info.patch
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+DEVICE_ROOT="$(realpath device/xiaomi/ginkgo)"
 
 if [ -f "vendor/google/pixel/Android.bp" ] && grep -q "SystemUIClocks-BigNum" "vendor/google/pixel/Android.bp"; then
   echo "Removing SystemUIClock modules from vendor/google/pixel..."
@@ -6,4 +7,14 @@ if [ -f "vendor/google/pixel/Android.bp" ] && grep -q "SystemUIClocks-BigNum" "v
   perl -0pe 's/android_app_import \{[^}]*name: "SystemUIClocks-[^}]*\}[^}]*\}//gms; s/\n{3,}/\n\n/g' "vendor/google/pixel/Android.bp" > "vendor/google/pixel/Android.bp.tmp" && mv "vendor/google/pixel/Android.bp.tmp" "vendor/google/pixel/Android.bp"
 
   echo "SystemUIClock modules removed."
+fi
+
+if ! grep -q "build_maintainer" "packages/apps/Settings/res/values/cm_strings.xml"; then
+  echo "Applying Settings maintainer patch..."
+
+  (
+      cd "packages/apps/Settings"
+      git am -3 "$DEVICE_ROOT/patches/0001-Settings-Add-Maintainer-string-into-device-info.patch"
+  )
+  echo "Settings maintainer patch applied successfully."
 fi
