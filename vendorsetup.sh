77248bfd4d6b3e17a800b04e37b5dd36485ecb7a
Flopster101 <nahuelgomez329@gmail.com>
2025-09-29 22:56:31 -0300

    ginkgo: Tidy up patch groups
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,32 +9,25 @@ if [ -f "vendor/google/pixel/Android.bp" ] && grep -q "SystemUIClocks-BigNum" "v
   echo "SystemUIClock modules removed."
 fi
 
+echo "-> Checking for Settings patches to apply..."
 if ! grep -q "build_maintainer" "packages/apps/Settings/res/values/cm_strings.xml"; then
-  echo "Applying Settings maintainer patch..."
-
   (
       cd "packages/apps/Settings"
       git am -3 "$SCRIPTS_ROOT/patches/0001-Settings-Add-Maintainer-string-into-device-info.patch"
   )
-  echo "Settings maintainer patch applied successfully."
 fi
 
+echo "-> Checking for Launcher3 patches to apply..."
 if ! grep -q "FLAG_ACTIVITY_NEW_TASK" "packages/apps/Launcher3/src/com/android/launcher3/quickspace/QuickEventsController.java"; then
-  echo "Applying Launcher3 Quickspace crash fix..."
-
   (
     cd "packages/apps/Launcher3"
     git am -3 "$SCRIPTS_ROOT/patches/0001-Launcher3-Fix-QuickSpace-crash-by-adding-FLAG_ACTIVI.patch"
   )
-  echo "Launcher3 Quickspace crash fix applied successfully."
 fi
 
 if ! grep -q "Grid size settings" "packages/apps/Launcher3/res/values/cr_strings.xml"; then
-  echo "Applying Launcher3 grid size patch..."
-
   (
     cd "packages/apps/Launcher3"
     git am -3 "$SCRIPTS_ROOT/patches/0001-Launcher3-Allow-changing-app-drawer-and-home-screen-.patch"
   )
-  echo "Launcher3 grid size patch applied successfully."
 fi
