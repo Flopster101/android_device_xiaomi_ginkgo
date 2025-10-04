979d9b236a88264cd6be1e5862c6c50f8b878dee
Flopster101 <nahuelgomez329@gmail.com>
2025-10-04 01:46:58 -0300

    ginkgo: Hi SpeakerFX!
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- device.mk
+++ device.mk
@@ -26,6 +26,9 @@ PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
     $(DEVICE_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
+# Audio effects
+PRODUCT_PACKAGES += SpeakerFX
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2340
 TARGET_SCREEN_WIDTH := 1080
