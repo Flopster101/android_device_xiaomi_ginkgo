d9d00686e662534c9732fc37974014a26b552fff
Flopster101 <nahuelgomez329@gmail.com>
2026-06-16 21:45:47 -0300

    ginkgo: Explicitly enable new Lunaris GMS flags
    * see https://github.com/Lunaris-AOSP/android/commit/6b67f7f102496fe2e2db9c25476b50886e1c3523
    
    Signed-off-by: Flopster101 <nahuelgomez329@gmail.com>

--- lineage_ginkgo.mk
+++ lineage_ginkgo.mk
@@ -28,6 +28,8 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Lunaris
 PRODUCT_SYSTEM_PROPERTIES += ro.lunaris.maintainer=Flopster101
+TARGET_USE_MAPS := true
+TARGET_USE_FILES := true
 
 # MIUI camera support
 $(call soong_config_set,camera,package_name,com.android.camera)
